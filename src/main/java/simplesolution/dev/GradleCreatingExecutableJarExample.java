1766b3c272ae6066d85e71c50ec9351235626dd8
simplesolutiondev <simplesolution.devteam@gmail.com>
2019-05-01 21:47:25 +0700

    Gradle Creating Executable JAR File

---
+++ src/main/java/simplesolution/dev/GradleCreatingExecutableJarExample.java
@@ -0,0 +1,8 @@
+package simplesolution.dev;
+
+public class GradleCreatingExecutableJarExample {
+
+    public static void main(String... args) {
+        System.out.println("Hello SimpleSolution.dev");
+    }
+}
